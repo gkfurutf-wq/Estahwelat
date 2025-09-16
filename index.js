b522a865194c6efd1421c3c1b10cbe7094694009
gkfurutf-wq <gkfurutf@gmail.com>
2025-09-16 15:37:35 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -604,10 +604,7 @@ app.get('/', (req, res) => {
             padding: 20px;
             max-height: 400px;
             overflow-y: auto;
-            overflow-x: hidden;
             border: 2px solid #e9ecef;
-            word-break: break-all;
-            overflow-wrap: break-word;
         }
         
         .notification {
